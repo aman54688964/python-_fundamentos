ba4fb2322f21b46dab81996e327f063193a83d2c
Amanuel Ortiz <amanuelotiz@gmail.com>
2026-09-18 09:23:45 +0200

    first commit

---
+++ vearibles.py
@@ -0,0 +1,11 @@
+#declaration de una variable de tipo cadena 
+name= "Alice"
+#declaration of an integer variable 
+age= 30
+print("Name:", name)
+print("Age:", age)
+print(name, "is", age, "years old.")
+#change the value of the variable name
+name= "Bob"
+print("Name:", name)
+print("Age:", age)
