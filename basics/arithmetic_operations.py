bbaaac696c55e6893d7a9862570babbdcab39ce9
Amanuel Ortiz <amanuelotiz@gmail.com>
2026-09-25 09:56:10 +0200

    variable operations1

---
+++ basics/arithmetic_operations.py
@@ -0,0 +1,14 @@
+from operator import add
+
+
+num1 = 10
+num2 = 20 
+result = num1 + num2
+print(result)
+result = num1 * num2 
+print(result)
+# *is the multiplication operator in python
+result = num1 / num2
+print(result)
+result = num1 - num2
+print(result)
